8baea406427fda6a202932e60e73498c3c6f6c75
Bulbul Ahammed <mdbulbulahammed@gmail.com>
2023-02-28 23:44:57 +0600

    initial setup

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="items-center p-4 bg-neutral text-neutral-content text-center">
+          <p>Copyright &copy; Todo app</p>
+    </footer>
+  )
+}
+
+export default Footer
